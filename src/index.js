0c11b961a34bb25d0f9c60ae2ccb2bc8f17666f1
Carlo Ruaro <cruaro@protonmail.com>
2024-02-28 00:34:41 +0100

    Added tabs style

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { dishes, generateMenu } from './menu';
 import { info, generateContact } from './contact';
 
 const content = document.getElementById('content');
-const tabs = document.querySelectorAll('.tabs-btn');
+const tabs = document.querySelectorAll('.tab-btn');
 
 tabs.forEach(t =>
 	t.addEventListener('click', () => {
@@ -12,6 +12,14 @@ tabs.forEach(t =>
 		if (t.textContent === 'Home') content.appendChild(component());
 		if (t.textContent === 'Menu') content.appendChild(generateMenu());
 		if (t.textContent === 'Contact') content.appendChild(generateContact());
+
+		tabs.forEach(t => {
+			if (t.classList.contains('tab-active')) t.classList.remove('tab-active');
+			if (!t.classList.contains('tab-h')) t.classList.add('tab-h');
+		});
+
+		t.classList.add('tab-active');
+		t.classList.remove('tab-h');
 	}),
 );
 
